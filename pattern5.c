d5fb59ebea4ce3d5369d3cb1887231c25d1a8534
Lakshaysingal <singallakshay84@gmail.com>
2024-02-26 13:22:48 +0530

    Practise

---
+++ pattern5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i=n;i>=0;i--){
+        for(int j=n;j>i;j--){
+            printf("%d",n-j+1);
+        }
+        printf("\n");
+    }
+}
