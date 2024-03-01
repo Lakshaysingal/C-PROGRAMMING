8e883793e74dcb03af803af97f3af0402a9b30a4
Lakshaysingal <singallakshay84@gmail.com>
2024-03-01 23:24:11 +0530

    Practise

---
+++ pattern8.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i=10;i<=n;i+=4){
+        
+            for (int space=1;space<=(n-i)/2;space++){
+                printf(" ");
+            }
+            for (int j=1; j<=i; j++){
+                printf("*");
+           
+            }
+            printf("\n");
+
+            for(int space=1; space<=n;space+=4){
+                printf(" ");
+            }
+
+            for (int k=1; k<;k++){
+                printf("*");
+            }
+        
+        }
+
+         for (int i=n-2;i>=10;i-=4){
+        
+            for (int space=1;space<=(n-i)/2;space++){
+                printf(" ");
+            }
+            for (int j=1; j<=i; j++){
+                printf("*");
+           
+            }
+            printf("\n");
+
+
+
+
+        }
+
+
+
+    
+
+}
+
+
+
+    
---
+++ pattern9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int l(){
+    int i=0;
+    while(i<=5){
+    printf("*\n");
+    i++;}
+    int j=0;
+    while(j<=10){
+    printf("*");
+    j++;}
+}
+
+
+int a(){
+    int i=0;
+    
+}
+
+int main()
+{
+    // l();
+}
