074129c0b53bb9aa460ee4fb8a13730393cc9e07
Lakshaysingal <singallakshay84@gmail.com>
2024-04-01 11:42:23 +0530

    practise

--- arrayfunction.c
+++ arrayfunction.c
@@ -1,4 +1,26 @@
 #include<stdio.h>
+int max;
+int result(int a[],int n){
+     max=0;
+     for(int i=0;i<n;i++){
+    if(a[i]>max){
+        max=a[i];
+    }}
+    return max;
+}
 int main(){
+    int n;
+    printf("enter the no of students : ");
+    scanf("%d",&n);
+    int a[100];
+    printf("enter the no of marks of students\n");
+    for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    }
+
+int maxmarks= result(a,n);
+
+printf("highest marks is %d ",maxmarks);
+
     
 }
---
+++ arrayfunction2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+    int max1,max2;
+int result(int a[],int n,int b[]){
+     max1=0;
+     max2=0;
+     for(int i=0;i<n;i++){
+    if(a[i]>max1){
+        max1=a[i];
+    }
+    if(b[i]>max2){
+        max2=a[i];
+    }
+    }
+
+    return max1 + max2;
+}
+int main(){
+    int n;
+    printf("enter the size of array : ");
+    scanf("%d",&n);
+    int a[100],b[100];
+    printf("enter the value of  first array ");
+    for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    }
+    printf("enter the value of  second array ");
+    for(int i=0;i<n;i++){
+    scanf("%d",&b[i]);
+    }
+
+
+int maxvalue= result(a,n,b);
+
+printf("highest marks is %d ",maxvalue);
+
+    
+}
+
+
