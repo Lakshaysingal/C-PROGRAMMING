acb8fa54831967370203060f15c404ef1f3fb24c
Lakshaysingal <singallakshay84@gmail.com>
2024-04-03 09:20:22 +0530

    practice

---
+++ string.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>
+
+// void rev(char str[]) {
+//     for (int i = 6 ; i >= 0; i--) {
+//         printf("%c", str[i]);
+//     }
+// }
+
+// int main() {
+//     char str[7] = "LAKSHAY";
+//     rev(str);
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char a[100] = "LAKSHAY";
+//     printf("%s",strupr(strrev(a)));
+//     return 0;
+// }
