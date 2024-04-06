8933e8e355009584475e0be1e5c94acf88167008
Lakshaysingal <singallakshay84@gmail.com>
2024-04-06 22:19:40 +0530

    practise

--- patternfunction.c
+++ patternfunction.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 void pattern(int n){
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            printf("1 ");
+        }
+        else{
+            printf("0 ");
+        }
+        for(int j=2;j<=i;j++){
+            if((i+j)%2==0){
+                printf("1 ");
+            }
+            else{
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
 
 
 
@@ -8,8 +25,18 @@ void pattern(int n){
 
 int main(){
     int n;
+    printf("enter the no of rows : ");
     scanf("%d",&n);
+    if(n>=1 && n<=10){
+
     pattern(n);
+    }
+    else if(n>10){
+        printf("ERROR");
+    }
+    else {
+        printf("ERROR");
+    }
 
     return 0;
     
