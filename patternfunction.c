9cd843109da0d57f94a8cf90d35e44e2d80b6233
Lakshaysingal <singallakshay84@gmail.com>
2024-04-06 22:06:46 +0530

    practise

---
+++ patternfunction.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void pattern(int n){
+
+
+
+
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    pattern(n);
+
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+// For n = 4, the output of the code will be:
+
+// ```
+// 1 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// ```
