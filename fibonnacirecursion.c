1cbf74c3c85c54882aef2a9ea8395393a80095f3
Lakshaysingal <singallakshay84@gmail.com>
2024-04-15 13:17:36 +0530

    practise

---
+++ fibonnacirecursion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int fib(int n){
+    if (n==0){
+        return 0;
+    }
+    else if( n==1){
+        return 1;
+    }
+    else{
+        return fib
+    }
+    
+}
+
+int main(){
+    int main(){
+    int n;
+    scanf("%d",&n);
+    int res;
+    res = fib(n);
+    printf("%d",res);
+}
+
+
+
+}
