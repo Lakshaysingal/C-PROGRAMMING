b0d75541990c69c3c557df9e91738e0b4ba0ad47
Lakshaysingal <singallakshay84@gmail.com>
2024-04-23 09:52:28 +0530

    practise

--- structures4.c
+++ structures4.c
@@ -9,10 +9,13 @@ struct employes{
 
 
 int main(){
+    int n;
+    printf("enter the no of employees : ");
+    scanf("%d",&n);
 
-    struct employes company[5];
+    struct employes company[n];
 
-    for(int i=0;i<3;i++){
+    for(int i=0;i<n;i++){
         printf("enter the name of employee : ");
         scanf(" %s",&company[i].name);
         printf("enter the id of employee : ");
@@ -23,6 +26,13 @@ int main(){
         scanf(" %d",&company[i].grosssalary);
         printf("enter the gender of employee : ");
         scanf(" %c",&company[i].gender);
+      
+    }
+    printf("previos salary : \n");
+    for(int i=0;i<n;i++){
+        printf("%d\n",company[i].grosssalary);
+    }
+    for(int i=0;i<n;i++){
         if(company[i].gender=='m' || company[i].gender == 'M'){
             company[i].grosssalary += (5*company[i].basesalary)/100;
         }
@@ -32,11 +42,12 @@ int main(){
     }
 
 
-    printf("%d",company[0].grosssalary);
-    printf("%d",company[1].grosssalary);
-    printf("%d",company[2].grosssalary);
-
+     printf("increment salary : \n");
+    for(int i=0;i<n;i++){
+        printf("%d\n",company[i].grosssalary);
+    }
 
 
 
 }
+// employee salary increment by structures 
