ac42e39a4d2d8d575572d22bf2585409e12022a1
Lakshaysingal <singallakshay84@gmail.com>
2024-04-24 22:38:42 +0530

    practise

--- viva.c
+++ viva.c
@@ -7,4 +7,21 @@ int main(){
     scanf("%d",&size);
     printf("enter the cost of brick : ");
     scanf("%d",&cost);
+
+    int maxbrick = (basearea/size);
+    
+    int i=1,a=1,b=1,c=0,totalbricks=0;
+
+    while(c<maxbrick){
+        totalbricks+=c;
+        a=b;
+        b=c;
+        c=a+b;
+        i++;
+    }
+
+    int totalcost = cost*totalbricks;
+    printf("no of brick in base layer %d\n",b);
+    printf("total no of brick %d",totalbricks);
+    printf("cost of brick %d",totalcost);
 }
