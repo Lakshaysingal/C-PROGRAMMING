80f301e7b71f8e1e2b1d267577af908fb55d63c5
Lakshaysingal <singallakshay84@gmail.com>
2024-04-24 22:21:11 +0530

    practise

---
+++ viva.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int basearea,size,cost;
+    printf("enter the base area : ");
+    scanf("%d",&basearea);
+    printf("enter the size of each brick : ");
+    scanf("%d",&size);
+    printf("enter the cost of brick : ");
+    scanf("%d",&cost);
+}
