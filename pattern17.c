a4eea34a66c8143eb72d0e989c17c375d3a6cf85
Lakshaysingal <singallakshay84@gmail.com>
2024-04-25 10:52:22 +0530

    practise

--- pattern17.c
+++ pattern17.c
@@ -4,13 +4,13 @@
 int main(){
 int n;
   scanf("%d",&n);
-  int i=1,count=1;
+  int i=1;
   while(i<=n){
     int j=1;
-    while(j<=n){
-      printf("%d ",count);
+    while(j<=i){
+      printf("* ");
       j++;
-      count++;
+      
     }
     printf("\n");
     i++;
@@ -22,9 +22,8 @@ int n;
 
 }
 
+// *
+// * *
+// * * *
+// * * * *
 
-// 1 2 3 4 5
-// 6 7 8 9 10
-// 11 12 13 14 15
-// 16 17 18 19 20
-// 21 22 23 24 25
