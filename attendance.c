24a60b97d32a65c77550d1afcc65f8f16f8f983e
Lakshaysingal <singallakshay84@gmail.com>
2024-04-27 00:06:12 +0530

    practise

--- attendance.c
+++ attendance.c
@@ -2,13 +2,16 @@
 int main(){
     int a,b;
     char ch;
-    scanf("%d%d",&a,&b);
+    printf("enter the total no of class conducted : ");
+    scanf("%d",&b);
+    printf("enter the no of class you are present : ");
+    scanf("%d",&a);
     float attend = ((float)a/b)*100;
     if(attend > 75){
         printf("allowed");
     }
     else{
-        printf("enter 'y' or 'n' if you have medical :  ");
+        printf("enter 'y' or 'n' if you have medical : ");
         scanf(" %c",&ch);
         if(ch=='y'|| ch=='Y'){
             printf("allowed");
