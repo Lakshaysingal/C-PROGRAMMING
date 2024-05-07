85936fb8bcfa5120c16d32fd92839b734f14732d
Lakshaysingal <singallakshay84@gmail.com>
2024-05-07 12:58:30 +0530

    practise

--- arraybubblesort.c
+++ arraybubblesort.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
 int main(){
     int n,arr[100];
+    scanf("%d",&n);
+
     for(int i=0;i<n;i++){
-        scanf("%d")
+        scanf("%d",&arr[i]);
     }
+
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
 }
+
+
+// bubble sort in array 
